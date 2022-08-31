954bae7062ba1c8b25db26e8d0f375d37f19d2eb
shivaramr <85446156+shivaramr@users.noreply.github.com>
2022-08-31 17:30:22 +0530

    my first commit

---
+++ src/components/Feed.jsx
@@ -0,0 +1,16 @@
+
+import React from "react";
+import { Box } from "@mui/material";
+import Post from "./Post";
+
+export default function Feed() {
+  return (
+    <Box flex={4} p={2}>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </Box>
+  );
+}
---
+++ src/theme.js
@@ -0,0 +1,16 @@
+// import { createTheme } from "@mui/material";
+
+// export const theme = createTheme({
+//     palette:{
+//         primary:{
+//             main: '#1760A5',
+//             light: 'skyblue',
+//         },
+//         secondary:{
+//             main:'#15C630',
+//         },
+//         otherColor:{
+//             main: '#999'
+//         }
+//     }
+// })
